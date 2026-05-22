99d1d9c750c93d257e75772acb923aede99c8bbd
venkatasrinivasak <venkatasrinivasa.k@worktual.com>
2026-05-22 16:04:53 +0530

    updated

--- src/store/redux-store/dispatch.ts
+++
@@ -1,6 +0,0 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
-import { AppDispatchType, RootState } from './root_store';
-
-export const useDispatcher: () => AppDispatchType = useDispatch;
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
-
